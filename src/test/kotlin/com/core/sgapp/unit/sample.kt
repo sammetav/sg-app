e2d8611cf5c8b51831cd7ba91056ac96895e91e8
sammetav <vishnu.sammeta@tset.com>
2023-04-15 18:20:48 +0200

    feat | fix test

---
+++ src/test/kotlin/com/core/sgapp/unit/sample.kt
@@ -0,0 +1,11 @@
+package com.core.sgapp.unit
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class sample {
+    @Test
+    fun sampleTest() {
+        Assertions.assertEquals(22, 11+11)
+    }
+}
